cec52344cf8166b986ff5ad216fae29304010ae8
MinatoSana <carljamesbalingit@gmail.com>
2020-05-29 16:12:58 +0800

    login

--- habit/webdeb/resources/views/welcome.blade.php
+++ habit/webdeb/resources/views/welcome.blade.php
@@ -1,100 +1,30 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+@extends('layouts.app')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+<div class="parallax" style="padding: 5%;"> 
+    <div style="padding: 9%; background-color:rgba(0,0,0,0.5); border-radius:10%;"> 
+    <img style="float:right;" src="https://media.istockphoto.com/vectors/vector-cartoon-of-detective-with-magnifying-glass-and-tracker-dog-vector-id595150482?k=6&m=595150482&s=170667a&w=0&h=MCzEMMRLZhfqNedF5frMMvobi0x1Ars8KsuAktjkgpM=" alt="">
+    <h2> Track your habits</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime molestias amet odio sapiente quibusdam doloremque animi accusamus! Non illo adipisci earum. Quaerat modi labore est sunt placeat excepturi alias nulla.</p>
+    <button class="btn btn-outline-success my-2 my-sm-0">Start Tracking Now</button>
+    </div>
+</div>
+<div class="dibide"> 
+    <img style="float:right;" src="https://media.istockphoto.com/vectors/vector-cartoon-of-detective-with-magnifying-glass-and-tracker-dog-vector-id595150482?k=6&m=595150482&s=170667a&w=0&h=MCzEMMRLZhfqNedF5frMMvobi0x1Ars8KsuAktjkgpM=" alt="">
+    <h2> Track your habits</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime molestias amet odio sapiente quibusdam doloremque animi accusamus! Non illo adipisci earum. Quaerat modi labore est sunt placeat excepturi alias nulla.</p>
+</div>
+<div class="parallax"> 
+    <div class="dibide">
+    <img style="float:left; margin: 2%; height: 40%; width: 35%;" src="/img/emot.jpg" alt="">
+    <h2>Rate your day!</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat ab itaque consequuntur debitis quam hic fugiat aspernatur quasi error facilis quos mollitia, minus enim ullam accusantium, laboriosam voluptatibus asperiores eum?</p>
+    </div>
+</div>
+<div class="dibide">
+    <img style="float:right; height: 40%; width: 50%;" src="/img/emot.jpg" alt="">
+    <h2>Rate your day!</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat ab itaque consequuntur debitis quam hic fugiat aspernatur quasi error facilis quos mollitia, minus enim ullam accusantium, laboriosam voluptatibus asperiores eum?</p>
+</div>
+@endsection
