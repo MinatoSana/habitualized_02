39283b2dcfd06bf622ccb93552b67c424affe168
MinatoSana <carljamesbalingit@gmail.com>
2020-06-16 15:01:02 +0800

    ty

--- webdeb/resources/views/home.blade.php
+++ webdeb/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Habits</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -16,7 +16,7 @@
                     @if(count($habits) > 0)
                     <table class="table table-striped">
                         <tr>
-                            <th>Habits</th>
+                            <th>Description</th>
                             <th></th>
                             <th>Time Lapse</th>
                             <th></th>
